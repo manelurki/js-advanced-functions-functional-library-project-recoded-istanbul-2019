b60023a5fb8df7b9595f3a0d3396f6e950748128
manel turki <menelturki@gmail.com>
2020-03-24 11:26:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const fi = (function() {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-    
+    each: function(array,alert) {
+        for (let element in array){
+           alert(array[element])
+        }
+      return array  
+    },
